4a391438da7356ddcf98de45a2ddb7895fad2592
gpapava <giorgi.papava.00@gmail.com>
2021-08-18 14:26:42 +0400

    Added getting languages through rest api

--- firebaseConfig/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCc5WWk4-v4y-hhu0nT7UyPiUq2QZ9WK70',
-  authDomain: 'ouchi-finance.firebaseapp.com',
-  projectId: 'ouchi-finance',
-  storageBucket: 'ouchi-finance.appspot.com',
-  messagingSenderId: '704737243837',
-  appId: '1:704737243837:web:8a283a2820bc55f3f63dd4',
-  measurementId: 'G-GYDYVRNSM5',
-};
